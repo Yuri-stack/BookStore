506cab0538ffddf7286435aa8ee48b7ad9cfebd4
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-05-26 22:50:07 -0300

    Feat. First Commit

---
+++ src/main/java/com/example/bookstoreManager/BookstoreManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookstoreManager/BookstoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
